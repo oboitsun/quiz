5713b491938fdedf4da98c76691f8ee979823cee
Oleksandr Boitsun <thewip3out@gmail.com>
2021-02-15 16:55:11 +0200

    check it'

--- script.js
+++ script.js
@@ -46,26 +46,41 @@ let qContent = document.getElementById('qContent')
 let qContainer = document.getElementById('qContainer')
 let bContainer = document.getElementById('bContainer')
 let quizChart = document.getElementById('quizChart')
+let chartLegends = document.getElementById('chartLegends')
+let chartLetter = [...document.getElementsByClassName('chart-letter')]
 let quizFinish = document.getElementById('quizFinish')
-let qIndex = 0
+let fCategory = document.getElementById('fCategory')
+let button = [...document.getElementsByClassName('button')]
+let qIndex = 25
 function StartQuiz(qObj) {
   let questionArr = []
 
   quizChart.style.height = '0'
+  chartLegends.style.display = 'none'
   qContainer.style.display = 'flex'
   bContainer.style.display = 'flex'
   quizFinish.style.display = 'none'
   for (let val of Object.values(qObj)) {
     questionArr.push(val)
   }
-
-  return questionArr.flat()
+  questionArr = questionArr.flat()
+  questionArr = questionArr.sort((q) => Math.random() - 0.5)
+  return questionArr
 }
 function questionChanges(i, qArray) {
+  if (i === 0) {
+    let whichIs = Object.keys(hardCodedQuestions).find((key) => {
+      let qst = hardCodedQuestions[key].indexOf(questionArray[qIndex])
+      if (qst > -1) return key
+    })
+    let currBar = bars.find((it) => it.type === whichIs)
+    button.forEach((it) => (it.style.background = currBar.color))
+  }
   document.getElementById('qTitle').textContent = `Question ${i + 1} of ${
     qArray.length
   }`
   document.getElementById('qContent').textContent = qArray[i]
+
   if (i >= qArray.length) {
     showfinishiQuiz()
   }
@@ -73,10 +88,11 @@ function questionChanges(i, qArray) {
 function questionInit(i, qArray, event) {
   let whichIs = Object.keys(hardCodedQuestions).find((key) => {
     let qst = hardCodedQuestions[key].indexOf(questionArray[qIndex])
-    console.log(qst)
     if (qst > -1) return key
   })
   let currBar = bars.find((it) => it.type === whichIs)
+  button.forEach((it) => (it.style.background = currBar.color))
+
   if (event && whichIs) currBar.setHeight(event.target.value)
   if (qIndex < qArray.length) {
     questionChanges(i + 1, qArray)
@@ -86,16 +102,24 @@ function questionInit(i, qArray, event) {
 
 function showfinishiQuiz() {
   qContainer.style.display = 'none'
-  quizChart.height = '1000px'
+  quizChart.height = '440px'
+  chartLegends.style.display = 'flex'
+  chartLegends.style.visibility = 'visible'
   bContainer.style.display = 'none'
-  quizChart.style.height = '150px'
-  quizChart.style.maxHeight = '150px'
+  quizChart.style.height = '100%'
+  quizChart.style.maxHeight = '350px'
   quizFinish.style.display = 'flex'
+  let getTop = bars.sort((a, b) => b.maxHeight - a.maxHeight)
+  document.getElementById(
+    'topBar'
+  ).textContent = `Your personality type is: \n\n ${getTop[0].type}`
+
   bars.forEach((it) => it.showHeight())
 }
 
-const Bar = function (id, type) {
+const Bar = function (id, type, color) {
   const bar = {}
+  bar.color = color
   bar.type = type
   bar.el = document.getElementById(id)
   bar.maxHeight = 5
@@ -110,12 +134,11 @@ const Bar = function (id, type) {
   return bar
 }
 const questionArray = StartQuiz(hardCodedQuestions)
-const bVis = Bar('bar-vis', 'VISIONARY')
-const bEnhance = Bar('bar-enhance', 'ENHANCER')
-const bProtect = Bar('bar-protect', 'PROTECTOR')
-const bPromote = Bar('bar-promote', 'PROMOTER')
-const bStr = Bar('bar-str', 'STRUCTURIST')
-const button = document.getElementsByClassName('button')
+const bVis = Bar('bar-vis', 'VISIONARY', '#fad0c4')
+const bEnhance = Bar('bar-enhance', 'ENHANCER', '#a1c4fd')
+const bProtect = Bar('bar-protect', 'PROTECTOR', '#f6d365')
+const bPromote = Bar('bar-promote', 'PROMOTER', '#f093fb')
+const bStr = Bar('bar-str', 'STRUCTURIST', '#38f9d7')
 let bars = [bVis, bEnhance, bStr, bProtect, bPromote]
 
 document.addEventListener('click', (event) => {
